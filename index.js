bfe435c1510bc1110e4c3dfb1110169c4b35abd4
Jon Garrity <jonnygarrity@gmail.com>
2021-07-06 15:34:49 -0500

    Made grid column and row size responsive to browser view size

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const gridContainer = document.querySelector('#gridContainer');
 gridContainer.style.display = 'grid';
 gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
+gridContainer.style.height = '100%';
 
 // create 16x16 grid of square divs
 for (i=1; i<=16; i++){
     for (j=1; j<=16; j++){
         const gridSquare = document.createElement('div');
         gridSquare.setAttribute('id', `box-${i}-${j}`);        
-        gridSquare.textContent = 'test'
+        gridSquare.textContent = 'test';
         gridContainer.appendChild(gridSquare);
-        console.log(gridSquare);
     }
 }
