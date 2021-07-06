82bdab8c8eff709ea51a3d9da4cac071eaf81a40
Jon Garrity <jonnygarrity@gmail.com>
2021-07-06 17:02:41 -0500

    Added reset sketch button functionality. Added ability for user to set sketch dimensions

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 const gridContainer = document.querySelector('#gridContainer');
 gridContainer.style.display = 'grid';
-gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
-gridContainer.style.height = '100%';
+
 // create 16x16 grid of square divs
-for (i=1; i<=16; i++){
-    for (j=1; j<=16; j++){
-        const gridCell = document.createElement('div');
-        gridCell.setAttribute('id', `box-${i}-${j}`);
-        gridCell.setAttribute('class', 'gridCell');
-        gridCell.textContent = 'test';
-        gridContainer.appendChild(gridCell);
+function drawGrid(gridSize){
+    gridSize = parseInt(gridSize);
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    for (i=1; i<=gridSize; i++){
+        for (j=1; j<=gridSize; j++){
+            const gridCell = document.createElement('div');
+            gridCell.setAttribute('id', `box-${i}-${j}`);
+            gridCell.setAttribute('class', 'gridCell');
+            gridContainer.appendChild(gridCell);
+        }
     }
 }
-// add listener for mouseover to color cell
-const gridCollection = gridContainer.querySelectorAll('div');
-gridCollection.forEach(gridCell => {
-    gridCell.addEventListener('mouseover', colorCell);    
-});
+function addListeners(){
+    const gridCollection = gridContainer.querySelectorAll('div');
+    gridCollection.forEach(gridCell => {
+        gridCell.addEventListener('mouseover', colorCell);    
+    });
+}
+
+drawGrid(16);
+addListeners();
+
 function colorCell(eventData){
     const selectedCell = gridContainer.querySelector(`#${eventData.target.id}`)
     selectedCell.classList.add('colored');
+}
+// reset and prompt for new grid dimensions
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener('click', resetView);
+function resetView(){
+    newGridSize = parseInt(prompt("What is the grid size? (max 100)"));
+    if (newGridSize && !isNaN(newGridSize) && newGridSize <= 100){
+        const gridToClear = document.getElementsByClassName('gridCell');
+        while(gridToClear.length > 0){
+            gridToClear[0].parentNode.removeChild(gridToClear[0]);
+        }
+        drawGrid(newGridSize);
+        addListeners();
+    }
 }
